b0c9935eecd1ce44a5f3107c4126a26a86640cd6
'hoil' <'ub7913@gmail.com'>
2020-09-16 18:02:15 +0900

    2020-09-16

---
+++ src/main/java/com/dev/model/MemberVO.java
@@ -0,0 +1,16 @@
+package com.dev.model;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private String id;
+	private String pw;
+	private String gender;
+	private String job;
+	private String reason;
+	private String mailyn;
+	private String hobby;
+	private String regdate;
+	
+}
